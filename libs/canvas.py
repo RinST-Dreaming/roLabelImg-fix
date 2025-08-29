56ee82b883c27fb1d477b2c857499a6bab29e641
RinST-Dreaming <544250926@qq.com>
2025-08-29 15:19:34 +0800

    better float issue fix

--- libs/canvas.py
+++ libs/canvas.py
@@ -592,11 +592,11 @@ class Canvas(QWidget):
             p.setPen(color)
             brush = QBrush(Qt.BDiagPattern)
             p.setBrush(brush)
-            p.drawRect(int(leftTop.x()), int(leftTop.y()), int(rectWidth), int(rectHeight))
+            p.drawRect(QRectF(leftTop.x(), leftTop.y(), rectWidth, rectHeight))
             
             #draw dialog line of rectangle
             p.setPen(self.lineColor)
-            p.drawLine(int(leftTop.x()), int(rightBottom.y()), int(rightBottom.x()), int(leftTop.y()))
+            p.drawLine(QLineF(leftTop.x(), rightBottom.y(), rightBottom.x(), leftTop.y()))
 
         self.setAutoFillBackground(True)
         if self.verified:
